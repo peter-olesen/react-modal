236419b400ee58be65ba74f09dcef1488ddabf54
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-08-26 09:26:02 +0200

    .

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,11 +3,15 @@ import s from './Modal.module.scss'
 export const Modal = ({ isOpen, onClose, children}) => {
     if (!isOpen) return null;
 
+    const innerClick = (e) => {
+        e.stopPropagation();
+    }
+
     return (
-        <div className={s.modalWrapper}>
-            <div className={s.innerModal}>
+        <div className={s.modalWrapper} onClick={onClose}>
+            <div className={s.innerModal} onClick={innerClick}>
                 {children}
-                
+
                 <button onClick={onClose}>
                     Close Modal
                 </button>
